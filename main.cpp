cb227dcf28882b8792592374b6f22a0583b9deb1
ChuMutt <chufilthymutt@gmail.com>
2024-03-03 10:31:06 -0600

    done

--- main.cpp
+++ main.cpp
@@ -4,31 +4,39 @@ using namespace std;
 int main()
 {
   char uin;
-  cout << "Do you think a stock is going to...\n"
+  cout << "This software does not claim to offer actual financial or\n"
+    << "investment advice, it is simply meant to serve as an example.\n"
+    << "Do not use this for actual investment advice,\n"
+    << "this program is merely a C++ example.\n\n";
+  cout << "Do you think a (hypothetical) stock is going to...\n"
     << "go (U)p\n"
     << "go (D)own\n"
     << "stay the same but (F)lat\n"
     << "stay the same but (V)olatile\n"
-    << "(Q)uit";
+    << "(Q)uit: ";
   cin >> uin;
   switch (uin) {
     case 'U':
-      cout << "If you think the stock is going UP, you should...";
+      cout << "If you think the stock is going UP, you should\n";
+      cout << "buy shares, buy a call, or (very risky) sell a put.\n";
       break;
     case 'D':
-      cout << "If you think the stock is going DOWN, you should...";
+      cout << "If you think the stock is going DOWN, you should\n";
+      cout << "buy a put (shorting), or (very risky) sell a call.\n";
       break;
     case 'F':
-      cout << "If you think the stock is going to stay the same but FLAT, you should...";
+      cout << "If you think the stock is going to stay the same but FLAT, you should\n";
+      cout << "employ a Short Straddle options strategy.\n";
       break;
     case 'V':
-      cout << "If you think the stock is going to stay the same but VOLATILE, you should...";
+      cout << "If you think the stock is going to stay the same but VOLATILE, you should\n";
+      cout << "employ a Long Straddle or Long Strangle options strategy.\n";
       break;
     case 'Q':
-      cout << "Quitting! Bye-bye.";
+      cout << "Quitting! Bye-bye.\n";
       return 0;
     default:
-      cout << "Error! Enter 'U', 'D', 'F', 'V', or 'Q'.";
+      cout << "Error! Enter 'U', 'D', 'F', 'V', or 'Q'.\n";
       break;
   }
   return 0;
